e1da5b35aa9f07cc28b26bdbdcb017a105a5f557
DanG-Mergin <dan.goldberg321@gmail.com>
2023-02-14 11:19:27 -0500

    debug and launch tasks working for data server

--- data/app/schema/inbound/_Request.py
+++ data/app/schema/inbound/_Request.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 from uuid import UUID, uuid4
-from typing import Dict, Optional
-from pydantic import BaseModel, Extra, Field, Json
-import sys
+from typing import Any, Dict, Optional
+from pydantic import BaseModel, Field, Json
 
-sys.path.append("..")
+# import sys
+# sys.path.append("..")
 
 
-class _Request(BaseModel, extra=Extra.ignore):
+class _Request(BaseModel):
     req_id: UUID = Field(default_factory=uuid4)
     orig_id: Optional[str]  # from the webfor example
     time_start: datetime = Field(default_factory=datetime.utcnow)
     time_end: Optional[datetime]
     data: Optional[Dict]
+
+    # TODO: add validators where appropriate
--- data/app/services/utils.py
+++ data/app/services/utils.py
@@ -1,6 +1,11 @@
 # utils.py is for SHARED abstract work
 from pydantic import BaseModel
 
+
 def cast_to_class(obj1: BaseModel, cls: BaseModel, **kwargs):
-    attr = attr = {k: v for k, v in obj1.__dict__.items()}
-    return cls(**attr, **kwargs)
+    if isinstance(obj1, BaseModel):
+        attr = attr = {k: v for k, v in obj1.__dict__.items()}
+    else:
+        kwargs = {**kwargs, **obj1}
+        return cls(**kwargs)
+    return cls(**kwargs, **attr)
